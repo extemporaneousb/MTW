312a9096d76a58d3f87aee33956f0cfd63fffff8
extemporaneousb <extemporaneousb@yahoo.com>
2025-01-22 22:43:10 -0800

    Added haplogroup_samples which calls haplogrep3 - if it hasn't been installed then it will install it.

---
+++ MTW/Tools/add_haplogroup.sh
@@ -0,0 +1,14 @@
+#! /bin/zsh
+
+
+mkdir -p haplogrep3-3.2.1
+pushd haplogrep3-3.2.1
+wget https://github.com/genepi/haplogrep3/releases/download/v3.2.1/haplogrep3-3.2.1-linux.zip
+unzip haplogrep3-3.2.1-linux.zip
+rm haplogrep3-3.2.1-linux.zip
+popd
+
+
+
+
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MTW", 
-    version="0.4.1", 
+    version="0.5.0", 
     packages=find_packages(), 
     include_package_data=True,           # Include non-code files like data, Makefile
     install_requires=[
@@ -14,11 +14,13 @@ setup(
         'MTW': [
             'Bin/Makefile',              # Include Makefile in package
             'Reference/*.fasta',         # Include all .fasta files from Reference/
+            'Tools/*',
         ],
     },
     scripts=[
         'MTW/Bin/process_samples',       
-        'MTW/Bin/summarize_samples',     
+        'MTW/Bin/summarize_samples',
+        'MTW/Bin/haplogroup_samples',
     ],
     author="James Bullard",
     description="The MTW package provides commands to compute and summarize heteroplasmy from Illumina paired-end sequencing data.",
